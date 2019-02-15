fe76097f18095059c8f6748219ae8b47245f174d
Emmanuel Price <mail@emmanuelprice.com>
2019-02-14 22:53:09 -0500

    Implements and fixes a AtvImg component for modern react. Need to figure out how to link the urls of the locally hosted pngs in the css.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import AtvImg from '../components/AtvImg'
+
+// 'http://kloc.pm/images/back.png',
+// 'http://kloc.pm/images/front.png',
 
 const Home = () => (
   <div>
-    Welcome!
+    <AtvImg
+      layers={[
+        's3://naturalbornchiller/redtree.png',
+        'https://s3.us-east-2.amazonaws.com/naturalbornchiller/ancient_being.png'
+      ]}
+      staticFallback="http://kloc.pm/images/kloc-icon-flattened.jpg"
+      isStatic={false}
+      className={'thisIsOptional'}
+      style={{ margin: '0 auto', width: 320, height: 190 }}
+    />
   </div>
 )
 
