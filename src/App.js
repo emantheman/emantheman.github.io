1906858eb5e366dfb3b6d9231609a84e60531c08
Emmanuel Price <mail@emmanuelprice.com>
2019-02-16 15:53:50 -0500

    Make parallaxCards flip on view-change. Use state in concert with css transform.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import routes from './config/routes'
 import './styles/App.scss'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       currentView: 'Home'
+    }
+  }
+  
   render() {
     const Routes = routes.map((route, index) => {
       const { path, view: View } = route
