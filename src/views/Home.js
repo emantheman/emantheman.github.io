28993f9eee67745dfc465e502a7647848d732951
Emmanuel Price <mail@emmanuelprice.com>
2019-02-16 20:01:54 -0500

    Must set state upstream, to make isStatic "true" until next component is loaded (and then not false again. Only way flipCard will work.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,31 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import AtvImg from '../components/AtvImg'
 import FlipCard from '../components/FlipCard'
 
 // '../../public/images/redtree.png',
 // '../../public/images/sun_zealot.png'
-const Home = () => (
-  <FlipCard 
-    card={{
-      front: {
-        layers: [
-          'http://kloc.pm/images/back.png',
-          'http://kloc.pm/images/front.png',
-        ],
-        staticFallback: "http://kloc.pm/images/kloc-icon-flattened.jpg",
-        isStatic: false
-      },
-      back: {
-        layers: [
-          'http://kloc.pm/images/front.png',
-          'http://kloc.pm/images/back.png',
-        ],
-        staticFallback: "http://kloc.pm/images/kloc-icon-flattened.jpg",
-        isStatic: false
-      }
-    }}
-    width={ 320 }
-    height={ 190 }/>
-)
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+
+  render() {
+    return (
+      <FlipCard 
+        card={{
+          front: {
+            layers: [
+              'http://kloc.pm/images/back.png',
+              'http://kloc.pm/images/front.png',
+            ],
+            staticFallback: "http://kloc.pm/images/kloc-icon-flattened.jpg",
+            isStatic: true
+          },
+          back: {
+            layers: [
+              'http://kloc.pm/images/back.png',
+              'http://kloc.pm/images/front.png',
+            ],
+            staticFallback: "http://kloc.pm/images/kloc-icon-flattened.jpg",
+            isStatic: true
+          }
+        }}
+        width={ 320 }
+        height={ 190 }/>
+    )
+  }
+}
 
 export default Home
