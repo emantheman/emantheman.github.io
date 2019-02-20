36e065c863f8db2667a040f3c88a8f7557bcc93c
Emmanuel Price <mail@emmanuelprice.com>
2019-02-20 11:33:08 -0500

    Tweaks Info=view styles

--- src/views/Info.js
+++ src/views/Info.js
@@ -13,14 +13,14 @@ class Info extends Component {
           ]}
           staticFallback={ 'http://kloc.pm/images/kloc-icon-flattened.jpg' }
           isStatic={ false }
-          style={{ width: 480, height: 285, float: 'left', marginRight: '10px' }}/>
+          style={{ width: 480, height: 285, float: 'left', marginRight: '11px' }}/>
         <h1>
           Emmanuel Price
         </h1>
         <hr width="260" align="left"/>
         <p>
           My two main focuses as an adult have been towards programming and meditation. While the two things seem unrelated at first, I see them as equal opposites. One (programming) requires a detective's eye for the abberant. The other (meditation) is concerned with erasing distinction.<br/><br/>
-          It is this paradox which most informs my work. Programming forces me to solve problems on an atomic scale, while meditation allows me to return easily and frequently to the big picture (a feature of this being that many "problems" need not be solved, simply removed).
+          It is this paradox which most informs my work: Programming forces me to solve problems on an atomic scale; meditation allows me to return easily and frequently to the big picture (a feature of this being that many "problems" need not be solved, simply removed).
         </p>
         <p>
           
