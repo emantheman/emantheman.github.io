0e792966e270b4b04ae53b1abde7c2392fa9a00b
Emmanuel Price <mail@emmanuelprice.com>
2019-02-20 14:53:36 -0500

    Lays groundwork for FileTree GUI.

--- src/components/FileTree.js
+++ src/components/FileTree.js
@@ -19,9 +19,36 @@ export default class FileTree extends Component {
 
     return (
       <div className="FileTree">
-        <span
-          className={'toggle ' + (treeHidden ? 'collapsed' : '')}
-          onClick={this.toggleTreeView}/>
+        {/* Hides and shows tree on click */}
+        <div className="toggle-container" onClick={this.toggleTreeView}>
+          <span className={'toggle ' + (treeHidden ? 'collapsed' : '')}/>
+        </div>
+        {/* Tree */}
+        <div className="branches">
+          <ul>
+            <li>Fruit
+              <ul>
+                <li>Red
+                  <ul>
+                    <li>Cherry</li>
+                    <li>Strawberry</li>
+                  </ul>
+                </li>
+                <li>Yellow
+                  <ul>
+                    <li>Banana</li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>Meat
+              <ul>
+                <li>Beef</li>
+                <li>Pork</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }
