51e37f5c94a00d3672706e49afc2707728c5b53d
Emmanuel Price <mail@emmanuelprice.com>
2019-03-01 09:43:01 -0500

    Adds design to preset

--- src/views/CGOL.js
+++ src/views/CGOL.js
@@ -12,7 +12,7 @@ const CGOL = () => (
         <p>
           <strong>Life</strong>, a 0-player game devised by mathematician John Horton Conway, is an example
           of "design and organization emerging in the absence of a designer." After providing an initial configuration,
-          known as the <i>seed</i> of the game, an observer can lean back and behold as patterns—<i>figures! structures!
+          known as the <i>seed</i> of the game, an observer can lean back and behold as patterns—<i>creatures! structures!
           symmatries!</i>—emerge briefly from chaotic soups to metastablize, lotus-like, atop puddles of mud.
           Successive generations of Conway's game are computed by applying simple rules to simple creatures.
         </p>
@@ -24,6 +24,7 @@ const CGOL = () => (
           <li>Any live cell with more than three live neighbors dies, as if by overpopulation.</li>
           <li>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
         </ol>
+        <span className="footnote"><sup>*</sup>A neighbor is defined as any horizontally, vertically, or diagonally adjacent square, giving each cell a total of 8 neighbors.</span>
         <h3>How It Works</h3>
         <p>
           The initial pattern constitutes the seed of the system. The first generation is created by applying
