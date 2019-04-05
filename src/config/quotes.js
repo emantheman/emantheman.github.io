9d9b259570a343a31ca9c06cc5b31e7da683a2ae
Emmanuel Price <mail@emmanuelprice.com>
2019-04-05 11:37:02 -0400

    Finishes quotes page; just need to add more quotes

--- src/config/quotes.js
+++ src/config/quotes.js
@@ -1,17 +1,23 @@
-export const quotes = [{
-  url: '',
-  author: '',
-  content: ''
+const quotes = [{
+  url: 'https://en.wikiquote.org/wiki/Antoine_de_Saint_Exup%C3%A9ry',
+  author: 'Antoine-Marie-Roger de Saint-Exupery',
+  content: 'Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.'
 }, {
-  url: '',
-  author: '',
-  content: ''
+  url: 'https://www.brainpickings.org/2011/12/02/steve-jobs-1995-life-failure/',
+  author: 'Steve Jobs',
+  content: 'The minute that you understand that you can poke life and actually something will, you know if you push in, something will pop out the other side, that you can change it, you can mold it. That’s maybe the most important thing. It’s to shake off this erroneous notion that life is there and you’re just gonna live in it, versus embrace it, change it, improve it, make your mark upon it.'
 }, {
-  url: '',
-  author: '',
-  content: ''
+  url: 'https://www.goodreads.com/quotes/5520-a-life-spent-making-mistakes-is-not-only-more-honorable',
+  author: 'George Bernard Shaw',
+  content: 'A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing.'
 }, {
-  url: '',
-  author: '',
-  content: ''
+  url: 'https://www.brainyquote.com/quotes/john_f_kennedy_108094',
+  author: 'John F. Kennedy',
+  content: 'Too often we enjoy the comfort of opinion without the discomfort of thought.'
+}, {
+  url: 'https://everydaypowerblog.com/lao-tzu-quotes/',
+  author: 'Lao Tzu',
+  content: 'A good traveler has no fixed plans and is not intent on arriving.'
 }]
+
+export default quotes
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,7 @@
 import Home from '../views/Home'
 import Me from '../views/Me'
 import CGOL from '../views/CGOL'
+import Quotes from '../views/Quotes'
 
 const routes = [{
   path: '/',
@@ -11,6 +12,9 @@ const routes = [{
 }, {
   path: '/projects/life',
   view: CGOL
+}, {
+  path: '/about/quotes',
+  view: Quotes
 }]
 
 export default routes
