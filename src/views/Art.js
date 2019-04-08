8d6d4d27b3a869b2b9d2447bccbd149534b68016
Emmanuel Price <mail@emmanuelprice.com>
2019-04-08 19:17:51 -0400

    Tweaks captions for artwork to include medium

--- src/views/Art.js
+++ src/views/Art.js
@@ -7,22 +7,22 @@ import '../styles/Art.scss'
 const IMAGES = [{
   src: 'https://lh3.googleusercontent.com/boJuDEZIizbUCSnLwPlD-oMV2wfk9Wh9chQiCabWOda0N_x2ZbRL9866Y1_XUmiOtgfoSCnn8mHDMVwD9qXwWQaVtemP_AmSkWirmvYiABKbShIxkvqos21FGjRoZh03Eyiu-oFxkw=w2400',
   thumbnail: 'https://lh3.googleusercontent.com/boJuDEZIizbUCSnLwPlD-oMV2wfk9Wh9chQiCabWOda0N_x2ZbRL9866Y1_XUmiOtgfoSCnn8mHDMVwD9qXwWQaVtemP_AmSkWirmvYiABKbShIxkvqos21FGjRoZh03Eyiu-oFxkw=w2400',
-  caption: 'Wanderer',
+  caption: 'Wanderer (16-bit)',
   orientation: 'square'
 }, {
   src: 'https://lh3.googleusercontent.com/NIwrNyz5soiMxo3r2U6eftM2vhNMZNIRpEzL-cF_MPX37mI_ZSn2WOvpZX_Cv1TAGnxgPK7PBWWrnX2I1UOOlr3Fbp_P5XNLI1c4NGQaE1YES9uqsdygXeXcGxz6M0lrQi-W6xzNwA=w2400',
   thumbnail: 'https://lh3.googleusercontent.com/NIwrNyz5soiMxo3r2U6eftM2vhNMZNIRpEzL-cF_MPX37mI_ZSn2WOvpZX_Cv1TAGnxgPK7PBWWrnX2I1UOOlr3Fbp_P5XNLI1c4NGQaE1YES9uqsdygXeXcGxz6M0lrQi-W6xzNwA=w2400',
-  caption: 'Woodland cretin',
+  caption: 'Woodland creature (16-bit)',
   orientation: 'square'
 }, {
   src: 'https://lh3.googleusercontent.com/dxdor_GpNGqNnHEJZP62L2yAtnUicsNZozKZPsN4ia2jcqrnsUa82UNySplXWHCND80V3QNdzwYYbQJi6CXmkQYTgAgzzUFMLdmg6mvlYAhUn9BpCBvqjWegzgi1i2-s67BSzUcpZA=w2400',
   thumbnail: 'https://lh3.googleusercontent.com/dxdor_GpNGqNnHEJZP62L2yAtnUicsNZozKZPsN4ia2jcqrnsUa82UNySplXWHCND80V3QNdzwYYbQJi6CXmkQYTgAgzzUFMLdmg6mvlYAhUn9BpCBvqjWegzgi1i2-s67BSzUcpZA=w2400',
-  caption: 'Cherry tree',
+  caption: 'Cherry tree (16-bit)',
   orientation: 'square'
 }, {
   src: 'https://lh3.googleusercontent.com/x0OKFo7GiMSHTpA75rGwPTp-KSH6RGeEm0aKkeB1zyqHHOkiA3KY-MpjfhT-lIuU4-WqHLFuWEMagl8jSxFR1odrD9VTAvB2MrDKr40WFclj5xVQNdwtNPnHnlWVxestqc4onXU0gQ=w2400',
   thumbnail: 'https://lh3.googleusercontent.com/x0OKFo7GiMSHTpA75rGwPTp-KSH6RGeEm0aKkeB1zyqHHOkiA3KY-MpjfhT-lIuU4-WqHLFuWEMagl8jSxFR1odrD9VTAvB2MrDKr40WFclj5xVQNdwtNPnHnlWVxestqc4onXU0gQ=w2400',
-  caption: 'Self',
+  caption: 'Self (pencil)',
   orientation: 'square'
 }]
 
