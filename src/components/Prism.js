b37220aee1dd6d9aaabbf39d9d9202cd4918792e
Emmanuel Price <mail@emmanuelprice.com>
2019-04-22 20:02:36 -0400

    Implements default props and inline styles for Prism.
    Uses aphrodite to style Prism dynamically within the class, so that
    any changes to props affect css and js symmetrically.

--- src/components/Prism.js
+++ src/components/Prism.js
@@ -1,82 +1,177 @@
 import React, { Component } from 'react'
 
-import '../styles/Prism.scss'
+import { StyleSheet, css } from 'aphrodite/no-important'
 
-export default class Prism extends Component {
+// Prism spin animation
+const spinKeyframes = {
+  '0%': { transform: 'rotateX(0)' },
+  '5%': { transform: 'rotateX(-90deg)' },
+  '25%': { transform: 'rotateX(-90deg)' },
+  '30%': { transform: 'rotateX(-180deg)' },
+  '50%': { transform: 'rotateX(-180deg)' },
+  '55%': { transform: 'rotateX(-270deg)' },
+  '75%': { transform: 'rotateX(-270deg)' },
+  '80%': { transform: 'rotateX(-360deg)' },
+  '100%': { transform: 'rotateX(-360deg)' }
+}
+
+class Prism extends Component {
   constructor(props) {
     super(props)
   
     this.state = {
-      descriptors: [
-        'meditator',
-        'book-reader',
-        'creative',
-        'hiker',
-        'mentor',
-        'visionary',
-      ],
-      affixed: 'web developer',
       i: 0,
       j: 1, 
       k: 2
     }
   }
 
+  /**
+   * Takes css value in the form '<Number><Units>' and returns ['<Number>', '<Units>'].
+   * 
+   * E.g., '100px' --> ['100', 'px']
+   * @param {String} css - css value to be parsed.
+   */
+  parseCSSVal = css => {
+    const cssVal = css.split('').filter(ch => isNaN(ch) === false).join(''), // filter: is a Number
+          cssUnit = css.split('').filter(ch => isNaN(ch) === true).join('')  // filter: is Not a Number
+    return [cssVal, cssUnit]
+  }
+
+  styles = () => {
+    let {
+      fontSize,
+      positionRight,
+      positionBottom,
+      positionLeft,
+      positionTop,
+      height,
+      width,
+      spinRate,
+      startDelay,
+      prismBorder,
+      prismBoxShadow,
+      backgroundColor,
+      fontColor
+    } = this.props
+
+    // defaults
+    spinRate = `${spinRate * 4}s`
+    startDelay = startDelay + 's'
+
+    // break val up into number and unit, e.g., [ '100', 'px' ]
+    const [ heightValue, heightUnits ] = this.parseCSSVal(height)
+    // halfs value and combines with units
+    const halfHeight = heightValue / 2 + heightUnits
+
+    // styles
+    const styles = StyleSheet.create({
+      Prism: {
+        position: 'absolute',
+        perspective: '800px',
+        perspectiveOrigin: `50% ${halfHeight}`,
+        right: positionRight,
+        left: positionLeft,
+        bottom: positionBottom,
+        top: positionTop,
+        fontSize
+      },
+      spinner: {
+        animationName: [spinKeyframes],
+        animationIterationCount: 'infinite',
+        animationTimingFunction: 'ease',
+        animationDuration: spinRate,
+        animationDelay: startDelay,
+        transformOrigin: `0 ${halfHeight}`,
+        margin: '0 auto',
+        position: 'relative',
+        transformStyle: 'preserve-3d',
+        width: width
+      },
+      side: {
+        position: 'absolute',
+        border: `1px solid ${prismBorder}`,
+        boxShadow: `inset 0 0 20px ${prismBoxShadow}`,
+        lineHeight: '1.1em',
+        paddingLeft: '7px',
+        color: fontColor,
+        backgroundColor,
+        width,
+        height
+      },
+      face1: {
+        transform: `translateZ(${halfHeight})`
+      },
+      face2: {
+        transform: `rotateX(-270deg) translateY(-${halfHeight})`,
+        transformOrigin: 'top center'
+      },
+      face3: {
+        transform: `translateZ(-${halfHeight}) rotateX(180deg)`
+      },
+      face4: {
+        transform: `rotateX(-90deg) translateY(${halfHeight})`,
+        transformOrigin: 'bottom center'
+      }
+    })
+    return styles
+  }
+
   componentDidMount() {
+    const { startDelay } = this.props
     setTimeout(() => {
       this.startCycle()
-    }, 3000)
+    }, startDelay * 1000)
   }
 
   /**
    * Starts cycling through descriptors.
    */
   startCycle = () => {
+    const { spinRate } = this.props
     // calls cycle descriptors,
     this.cycleDescriptors()
     // waits until cycle has completed (10 sec),
     setTimeout(() => {
       // recurses.
       this.startCycle()
-    }, 10000)
+    }, spinRate * 4000)
   }
 
   /**
    * Cycles through descriptors.
    */
   cycleDescriptors = () => {
+    const maxLen = this.props.descriptors.length
     // Shifts each index up to a new descriptor
     this.setState(prevState => {
-      const { i, j, k, descriptors } = prevState
+      const { i, j, k } = prevState
       return { 
-        i: (k + 1) % descriptors.length,
-        j: (j + 3) % descriptors.length,
-        k: (i + 5) % descriptors.length
+        i: (k + 1) % maxLen,
+        j: (j + 3) % maxLen,
+        k: (i + 5) % maxLen
       }
     })
   }
 
-
   render() {
-    const {
-      descriptors,
-      affixed,
-      i, j, k
-    } = this.state
+    const { i, j, k } = this.state
+    const { affixed, descriptors } = this.props
+    const { Prism, spinner, side, face1, face2, face3, face4 } = this.styles()
 
     return (
-      <div className="Prism">
-        <div className="spinner">
-          <div className={"side face1"}>
+      <div className={css(Prism)}>
+        <div className={css(spinner)}>
+          <div className={css(side, face1)}>
             { affixed }.
           </div>
-          <div className={"side face2"}>
+          <div className={css(side, face2)}>
             { descriptors[i] }.
           </div>
-          <div className={"side face3"}>
+          <div className={css(side, face3)}>
             { descriptors[j] }.
           </div>
-          <div className={"side face4"}>
+          <div className={css(side, face4)}>
             { descriptors[k] }.
           </div>
         </div>
@@ -84,3 +179,21 @@ export default class Prism extends Component {
     )
   }
 }
+
+Prism.defaultProps = {
+  fontSize: '50px',
+  fontColor: 'white',
+  backgroundColor: 'salmon',
+  positionRight: '-58px',
+  positionTop: '6px',
+  positionLeft: 'unset',
+  positionBottom: 'unset',
+  height: '60px',
+  width: '370px',
+  spinRate: 2.5,
+  startDelay: 3,
+  prismBorder: 'rgba(245, 117, 103, 0.707)',
+  prismBoxShadow: 'rgba(247, 158, 158, 0.351)',
+}
+
+export default Prism
--- src/views/Home.js
+++ src/views/Home.js
@@ -5,20 +5,19 @@ import Prism from '../components/Prism'
 
 import '../styles/Home.scss'
 
-export default class Home extends Component {
-  DESCRIPTORS = [
-    'Web Developer',
-    'Meditator',
-    'Book Reader',
-    'Writer',
-    'Creative',
-    'Hiker',
-    'Mentor',
-    'Visionary',
-  ]
+const DESCRIPTORS = [
+  'meditator',
+  'book-reader',
+  'creative',
+  'hiker',
+  'mentor',
+  'visionary',
+]
 
+export default class Home extends Component {
   componentWillUnmount() {
     this.props.menu.unstow()
+    // add class which includes hide-animation.
   }
 
   render() {
@@ -30,7 +29,11 @@ export default class Home extends Component {
             to="/info/bio">
             Manny Price, 
           </Link>
-          <Prism/>
+          <Prism
+            descriptors={ DESCRIPTORS }
+            affixed="web developer"
+            spinRate={ 2.5 }
+            startDelay={ 3 }/>
         </div>
       </div>
     )
