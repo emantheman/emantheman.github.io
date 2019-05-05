34f67e6d80180c3f1acd9703fbedcf5a1da83f56
Emmanuel Price <mail@emmanuelprice.com>
2019-05-05 16:15:01 -0400

    Updates

---
+++ precache-manifest.1b6f481ceca1cefacd8546a13c03386e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "e5c099ef7f466899db0c",
+    "url": "/portfolio/static/js/main.e5c099ef.chunk.js"
+  },
+  {
+    "revision": "e37b405f0c686710082d",
+    "url": "/portfolio/static/js/2.e37b405f.chunk.js"
+  },
+  {
+    "revision": "e5c099ef7f466899db0c",
+    "url": "/portfolio/static/css/main.e8e640a7.chunk.css"
+  },
+  {
+    "revision": "fa3fb26f438ff6ff7e72d2ae3a3ee640",
+    "url": "/portfolio/index.html"
+  }
+];
